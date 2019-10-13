ae6f9959272f472ae50ff32db760d95c753b9a55
rilika <53854591+rilika@users.noreply.github.com>
2019-10-13 16:07:15 +0530

    Update kth_smallest.c

--- kth_smallest.c
+++ kth_smallest.c
@@ -1,32 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 int main()
 {
-int i,j,n,t,min,k;
-printf("Enter the no. of element:");
-scanf("%d",&n);
-int a[n];
-printf("Enter k");
-
-scanf("%d",&k);
-printf("Enter the element:");
-for(i=0;i<n;i++)
-{
-
-scanf("%d",&a[i]);
-}
-
-for(i=0;i<=k-1;i++)
-{
-min=i;
-for(j=i+1;j<=n-1;j++)
-{
-if(a[j]<a[min])
-min=j;
-}
-t=a[i];
-a[i]=a[min];
-a[min]=t;
-}
-printf("The kth minimum is:%d\n",a[k-1]);
-return(0);
+    int a[50],n,i,k,j,temp,s=a[0];
+    printf("Enter the no of elements");
+    scanf("%d",&n);
+    printf("enter the elements");
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("enter the value of k");
+    scanf("%d",&k);
+    for(i=0;i<k-1;i++)
+    { 
+        printf("%d",a[i]);
+    }
+    return 0;
 }
