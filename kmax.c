e16177d3da7c64613f60faed74a2f7fb32920a83
rilika <53854591+rilika@users.noreply.github.com>
2019-10-13 16:19:01 +0530

    Update kmax.c

--- kmax.c
+++ kmax.c
@@ -1,43 +1,32 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
+#include<conio.h>
 int main()
 {
-int n,k;
-printf("Enter the no. of element:");
-scanf("%d",&n);
-int a[n];
-printf("Enter k");
+    int a[50],n,i,k,j,temp;
+    printf("Enter the no of elements");
+    scanf("%d",&n);
+    printf("enter the elements");
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("enter the value of k");
+    scanf("%d",&k);
+for(i=n-1;i>=k;i--)
+       {
 
-scanf("%d",&k);
-printf("Enter the element:");
-for(int i=0;i<n;i++)
-{
+        printf("%d",a[i]);}
 
-scanf("%d",&a[i]);
-}
-
-for(int i=0;i<n-1;i++)
-{
-for(int j=i+1;j<n;j++)
-{
-if(a[i]>a[j])
-{
-int temp=a[i];
-a[i]=a[j];
-a[j]=temp;
-}
-}
-
-}
-
-int i=n-1;
-printf("k largest elements:");
-for(int j=1;j<=k;j++)
-{
-printf("%d\n",a[i]);
-i--;
-}
-return 0;
+    return 0;
 }
 
