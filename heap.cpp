a81fa076970397916da0b098bcbd49241397fdaf
rilika <53854591+rilika@users.noreply.github.com>
2019-10-24 14:29:24 +0530

    Create heap.cpp

---
+++ heap.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+#include <limits.h> 
+  
+
+bool isHeap(int arr[], int i, int n) 
+{ 
+// If a leaf node 
+if (i > (n - 2)/2) 
+    return true; 
+  
+
+if (arr[i] >= arr[2*i + 1] && arr[i] >= arr[2*i + 2] && 
+    isHeap(arr, 2*i + 1, n) && isHeap(arr, 2*i + 2, n)) 
+    return true; 
+  
+return false; 
+} 
+  
+
+int main() 
+{ 
+    int arr[] = {90, 15, 10, 7, 12, 2, 7, 3}; 
+    int n = sizeof(arr) / sizeof(int)-1; 
+  
+    isHeap(arr, 0, n)? printf("Yes"): printf("No"); 
+  
+    return 0; 
+}
